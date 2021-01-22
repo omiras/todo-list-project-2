9c328de67be1b6019177f45f3b19e884ab281411
oskytar <oscar.miras@gmail.com>
2021-01-22 12:09:49 +0100

    updated using methods to update the task

--- js/app.js
+++ js/app.js
@@ -1,54 +1,65 @@
-import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js' // JAvaScript ES6 y tiene que ver con trabajar de manera modular con clases 
 
 const app = Vue.createApp({
     data() {
         return {
-            items: [],
-            currentItem: ''
+            items: [], // listado de tareas
+            currentItem: '', //texto actual del input 
         }
     },
     methods: {
+        getItemById(id) {
+            return this.items.find(item => {
+                return item.id == id
+            })
+        },
         addItem() {
             this.items.push({
-                id: nanoid(),
-                task: this.currentItem,
-                isCompleted: false,
-                editMode : false
+                id: nanoid(), // identificador único
+                task: this.currentItem, // texo que hemos introducido en el input
+                editMode: false, // Es bastante habitual controlar con algún tipo de mecanismo si estamos en modo edición o no
+                editingTaskText: this.currentItem
             })
             this.currentItem = ''
         },
-        findItemById(id) {
-            return this.items.find(item=> item.id == id)
-
+        deleteItem(id) {
+            console.log(id)
+            // Actualizar el estado de mi app, para eliminar la tarea identificada con este id
+           this.items = this.items.filter(item =>{
+               return item.id != id
+           })
         },
-        removeItem(id) {
+        completeItem(id) {
+            // Buscar este elemento en el array de items
+            // Modificar la propiedad de dicho elemento 'done' a false
+            let item = this.getItemById(id)
+            item.done = !item.done
+        },
+        clearItems() {
+            // Quiero actualizar mi array de items para quedarme solo con aquellos que NO están completados aún 
             this.items = this.items.filter(item => {
-                return item.id != id
+                return !item.done
             })
         },
-        taskCompleted(id) {
-            let item = this.findItemById(id)
-            item.isCompleted = true
-        },
         editItem(id) {
-            let item = this.findItemById(id)
+            // Vamos poner el item identificado con este id en modo edición
+            let item = this.getItemById(id)
+
             item.editMode = true
         },
         taskEdited(id) {
-            let item = this.findItemById(id)
+
+            // Disclaimer!! Sería más adecuado recuperar el  id y el nuevo valor actualizado, y modificarlo en el array de tareas
+            let item = this.getItemById(id)
+            item.task = item.editingTaskText
             item.editMode = false
 
-        },
-        removeTasksFinished() {
-            this.items = this.items.filter(item => {
-                return !item.isCompleted
-            })
         }
     },
     computed: {
-        mustDisable() {
+        isListEmpty() {
             return this.items.length == 0
-        }
+        },
     }
 })
 
